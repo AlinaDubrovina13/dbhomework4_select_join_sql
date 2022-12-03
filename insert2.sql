fb6c768cb81aec22fc295f641807f5608cb37448
Alina Dubrovina <avreya13@mail.ru>
2022-12-04 00:47:23 +0400

    insert

--- insert2.sql
+++ insert2.sql
@@ -44,6 +44,9 @@ insert into album values
 (7, 'Follow the reaper', 2000),
 (8, 'Far beyond driven', 1994)
 
+insert into album values
+(9, 'Spirit', 2018)
+
 insert into musician_album values
 (1, 1),
 (2, 2),
@@ -65,8 +68,66 @@ insert into tracks values
 (8, 'Blind', 4, 3),
 (9, 'The last day of summer', 5, 4),
 (10, 'Precious', 4, 5),
-(11, )
+(11, 'Nothings impossible', 4, 5),
+(12, 'Shatter', 6, 6),
+(13, 'Kiss', 4, 6),
+(14, 'Every time i die', 4, 7),
+(15, 'Kissing the shadows', 4, 7),
+(16, '5 minutes alone', 5, 8),
+(17, 'i am broken', 3, 8)
+(18, 'My girlfriends girlfriend', 4, 1)
 
 insert into tracks values
 (19, 'Be my druidess', 5, 1)
+
+insert into collection values
+(1, 'Doom metal collection', 2020),
+(2, 'Dark romantic collection', 2000),
+(3, 'Alternative rock collection', 2005),
+(4, 'The best of synthpop', 2018),
+(5, 'Dark songs collection', 2015),
+(6, 'Trash and death metall collection', 2017),
+(7, 'Metal Ballads', 2022),
+(8, 'Old school of Rock', 2019)
+
+insert into tracks_collection values
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(2, 1),
+(2, 4),
+(2, 12),
+(2, 13),
+(2, 18),
+(3, 7),
+(3, 8),
+(3, 5),
+(3, 15),
+(3, 16),
+(4, 9),
+(4, 10),
+(4, 11),
+(5, 1),
+(5, 2),
+(5, 12),
+(5, 18),
+(5, 13),
+(6, 14),
+(6, 15),
+(6, 16),
+(6, 17),
+(7, 2),
+(7, 4),
+(7, 5),
+(7, 6),
+(7, 1),
+(8, 3),
+(8, 14),
+(8, 15),
+(8, 16),
+(8, 17)
+
 
